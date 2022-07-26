060ae99daa77b19648fd9efde1359175d86ea142
Duncan <duncan.moiyo@gmail.com>
2022-07-26 16:27:02 +0300

    Add get mapping to get all and by id on todo items & patch mapping

--- src/main/java/com/sareto/todoapi/controller/TodoController.java
+++ src/main/java/com/sareto/todoapi/controller/TodoController.java
@@ -3,12 +3,11 @@ package com.sareto.todoapi.controller;
 import com.sareto.todoapi.models.TodoModels;
 import com.sareto.todoapi.services.ITodoapiServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/todo")
@@ -23,4 +22,17 @@ public class TodoController {
     public TodoModels AddTodos(@RequestBody TodoModels todoItem){
         return todoapiServices.addTodoItem(todoItem);
     }
+    @GetMapping
+    public List<TodoModels> AllTodos(){
+        return todoapiServices.AllTodo();
+    }
+    @GetMapping("{id}")
+    public Optional<TodoModels> getTodoItemById(@PathVariable("id") Long id) throws  IllegalAccessException{
+        return todoapiServices.getTodoItemById(id);
+    }
+    @PatchMapping("{id}")
+    public TodoModels editTodoItem(@PathVariable("id") Long id, @RequestBody TodoModels todoItem){
+        return todoapiServices.updateTodoItems(id, todoItem);
+    }
+
 }
