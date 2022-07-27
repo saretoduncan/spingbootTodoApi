c2f6f9aea9827a37081d072eee61d6679679306e
Duncan <duncan.moiyo@gmail.com>
2022-07-27 15:33:51 +0300

    change swaggerfox to swagger-openapi

--- src/main/java/com/sareto/todoapi/TodoapiApplication.java
+++ src/main/java/com/sareto/todoapi/TodoapiApplication.java
@@ -2,10 +2,10 @@ package com.sareto.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class TodoapiApplication {
 	public static void main(String[] args) {
  		SpringApplication.run(TodoapiApplication.class, args);
